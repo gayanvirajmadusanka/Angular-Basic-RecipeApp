efa02235fba6729949968c74ed3fbdfd4389a485
Gayan Madusanka <gayan.virajs@gmail.com>
2020-10-12 10:18:47 +0530

    3. Course Project - The Basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,12 +11,12 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A Test Recipe',
       'This is simply a Test',
-      'E:ProjectsAngular-BasicAngular-Basic-RecipeAppsrcImagesMoroccan-Chickpea-Bowls-Recipe.jpg'
+      'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1335&q=80'
     ),
     new Recipe(
       'A Test Recipe',
       'This is simply a Test',
-      'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.immediate.co.uk%2Fproduction%2Fvolatile%2Fsites%2F30%2F2020%2F08%2Fchorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg%3Fquality%3D90%26resize%3D960%2C872&imgrefurl=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&tbnid=dOfrHuMDgw7-9M&vet=12ahUKEwjQl5Hrr6zsAhURYCsKHbiuDzkQMygAegUIARD1AQ..i&docid=0MYkDl3kvGAUTM&w=960&h=872&q=recipe%20images&client=firefox-b-d&ved=2ahUKEwjQl5Hrr6zsAhURYCsKHbiuDzkQMygAegUIARD1AQ'
+      'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1335&q=80'
     ),
   ];
 
